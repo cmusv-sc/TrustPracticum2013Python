d06cc146e022f0a6d2d01d3feb24e8859df15455
Chris Lee <chris@the-eye.(none)>
2014-04-03 10:58:47 -0700

    Clean up

--- db/scratch.py
+++ db/scratch.py
@@ -5,6 +5,9 @@ import sys
 
 import config
 
+def strip_quotes(s):
+  return re.sub('"','',s)
+
 if __name__ == '__main__':
   db = pymongo.MongoClient()[config.DB_NAME]
   print config.DB_NAME
@@ -13,10 +16,15 @@ if __name__ == '__main__':
     start = re.match('<((article)|(inproceedings)|(proceedings)|(book)|(incollection)|(phdthesis)|(mastersthesis)|(www))',line)
     end = re.match('</((article)|(inproceedings)|(proceedings)|(book)|(incollection)|(phdthesis)|(mastersthesis)|(www))',line)
     if start:
-      doc['type'] =  start.groups()[0]
+      doc['type'] =  strip_quotes(start.groups()[0])
       tag_attributes = re.findall(' (.+?)=(.+?)(?=[ >])',line)
       for attr in tag_attributes:
-        doc[attr[0]] = attr[1]
+        doc[attr[0]] = strip_quotes(attr[1])
+      #Sometimes, there's an extra tag on the same line.
+      extra_tag = re.match('.+?> <(.+?)>(.+?)</(.+?)>', line)
+      if extra_tag:
+        groups = extra_tag.groups()
+        doc[groups[0]] = strip_quotes(groups[1])
     elif end:
       db['documents'].insert(doc)
       doc = {}
@@ -28,10 +36,10 @@ if __name__ == '__main__':
         #Nested attributes. Need to clean out tag name
         if tag_attributes != [] and len(groups) == 2:
           tag_name = re.search('<(.+?) ',line).groups()[0]
-          sub_doc = {tag_name: groups[1]}
+          sub_doc = {tag_name: strip_quotes(groups[1])}
           for attr in tag_attributes:
-            sub_doc[attr[0]] = attr[1]
+            sub_doc[attr[0]] = strip_quotes(attr[1])
         elif len(groups) == 2:
-          doc[groups[0]] = groups[1]
+          doc[groups[0]] = strip_quotes(groups[1])
         else:
           print 'This should not happen'
