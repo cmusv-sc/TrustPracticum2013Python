f5a8c195438285531f7748092b7746c69f16a3a3
Chris Lee <chris@the-eye.(none)>
2014-04-07 11:51:54 -0700

    Added publication factor

--- trust/scratch.py
+++ trust/scratch.py
@@ -3,9 +3,16 @@
 __version__='Sp2014'
 
 import datetime
+import math
 import pymongo
 
 import config
+
+def _calculate_totals(val_list):
+  """
+  Takes a list of tuples and returns the sum of the second element in each tuple.
+  """
+  return sum(map(lambda (x): x[1], val_list))
 
 def _time_factor(date):
   """
@@ -22,24 +29,7 @@ def _time_factor(date):
   else:
     return config.tOld
 
-def _coauthorship_details(col, primary_author):
-  """
-  Given an author, returns a mapping from coauthors to coauthorship date.
-  """
-  coauthors = {}
-  for doc in col.find({'author': {"$in": [primary_author]}}):
-    authors = doc['author']
-    if isinstance(authors, list):
-      for author in authors:
-        if author != primary_author:
-          if author not in coauthors:
-            coauthors[author] = []
-          temp = coauthors[author]
-          temp.append(doc['mdate'])
-          coauthors[author] = temp
-  return coauthors
-
-def _coauthorship_details_map(col, primary_author, fun_handle):
+def _authorship_details_map(col, primary_author, fun_handle):
   """
   Map a function fun_handle over each document that primary_author authored.
   """
@@ -49,25 +39,31 @@ def _coauthorship_details_map(col, primary_author, fun_handle):
     return_vals.append(fun_handle(doc))
   return return_vals
 
-def _social_coauthorship_factor(col, primary_author):
+def _coauthorship_details_map(col, primary_author, fun_handle):
   """
-  Given an author, returns mapping from coauthors to social coauthorship factor (SCF). Given a partion of the time space P = {P_i}:
+  Map a function fun_handle over each author in each document that primary_author authored. Returns a list of tuples (coauthor, return value of fun_handle(coauthor)).
+  """
+  return_vals = []
+  for doc in col.find({'author': {"$in": [primary_author]}}):
+    authors = doc['author']
+    if isinstance(authors, list):
+      for author in authors:
+        if author != primary_author:
+          return_vals.append((author, fun_handle(doc)))
+  return return_vals
+
+def _publication_factor(doc):
+  date = doc['mdate']
+  doc_type = doc['type']
+  return _time_factor(date)*config.type2weights[doc_type]
+  
+def _social_coauthorship_factor(doc):
+  """
+  Given a document, return the time factor. Passed into the map function and summed, the end goal becomes:
+  Given an author, return SCF.
   SCF = \sum_{P_i} (#coauthorships in P_i) (scale factor for P_i)
   In this particlar implementation, we partition the space into recent, intermediate, and old.
   """
-  coauthor2scf = {}
-  coauthor_details = _coauthorship_details(col, primary_author)
-  for coauthor in coauthor_details:
-    scf = 0
-    for date in coauthor_details[coauthor]:
-      scf = scf + _time_factor(date)
-    coauthor2scf[coauthor] = scf
-  return coauthor2scf
-
-def _publication_factor(col, primary_author):
-  pass
-
-def _social_coauthorship_factor_mapper(doc):
   date = doc['mdate']
   return _time_factor(date)
 
@@ -76,5 +72,5 @@ if __name__ == '__main__':
   col = db[config.COLLECTION_NAME]
 #  print _coauthorship_details(col, "Rajni Goel")
 #  print _coauthorship_details(col, "Massimo Zancanaro")
-  print sum(_social_coauthorship_factor(col, "Massimo Zancanaro").values())
-  print sum(_coauthorship_details_map(col, "Massimo Zancanaro", _social_coauthorship_factor_mapper))
+  print _calculate_totals(_coauthorship_details_map(col, "Massimo Zancanaro", _social_coauthorship_factor))
+  print math.ceil(_calculate_totals(_coauthorship_details_map(col, "Massimo Zancanaro", _publication_factor)))
