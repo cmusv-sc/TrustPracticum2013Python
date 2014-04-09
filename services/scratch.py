271c6bc303f16f7125636af714e589315a30ec3d
Chris Lee <chris@the-eye.(none)>
2014-04-09 14:35:15 -0700

    Web service for trust

--- services/scratch.py
+++ services/scratch.py
@@ -5,15 +5,27 @@ import ujson
 import json
 import requests
 import re
+import trust.scratch
 
 HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 9000 # Maybe set this to 9000.
+
+def _handle_reputation(s):
+    print s.path
+    m = re.search('getReputation/(.+?)/',s.path)
+    if m:
+        name = re.sub('%20',' ',m.groups()[0])
+    else:
+        m = re.search('getReputation/(.+)',s.path)
+        name = re.sub('%20',' ',m.groups()[0])
+    s.wfile.write("<p>Total trust for {0}: {1}".format(name,trust.scratch.get_total_trust(name)))
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
+
     def do_GET(s):
         """Respond to a GET request."""
         s.send_response(200)
@@ -23,9 +35,13 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.wfile.write("<body><p>This is a test.</p>")
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
-        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-        s.wfile.write("</body></html>")
+        if re.search("getReputation",s.path):
+            _handle_reputation(s)
+        else:
+            s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+            s.wfile.write("</body></html>")
 
+            
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
