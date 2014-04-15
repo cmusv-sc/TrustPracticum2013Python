43c14037a58ccbb73c16f9e5c1680994521f5189
Chris Lee <Chris.Lee@sv.cmu.edu>
2014-04-14 22:55:57 -0700

    *** empty log message ***

--- ml/scratch.py
+++ ml/scratch.py
@@ -38,9 +38,10 @@ if __name__ == '__main__':
   col = db[config.COLLECTION_NAME]
   s1 = set(_flatten_list(authorship_details_map(col, 'Luis Ramos', _get_jaccard_info)))
   s2 = set(_flatten_list(authorship_details_map(col, 'Shahram Ghandeharizadeh', _get_jaccard_info)))
-  print s1.union(s2)
-  print s1.intersection(s2)
-  print jaccard_similarity(col, 'Luis Ramos', 'Shahram Ghandeharizadeh')
+#  print s1.union(s2)
+#  print s1.intersection(s2)
+#  print jaccard_similarity(col, 'Luis Ramos', 'Shahram Ghandeharizadeh')
+  print authorship_details_map_before_year(col, 'Luis Ramos', lambda(x): x, 2000)
   #db.documents.find('this.year < 1999 && this.author.indexOf("Luis Ramos") > -1')
   
 
