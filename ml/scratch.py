03b6506c53f8a8a45475eb1f1f3c783520235f37
Chris Lee <Chris.Lee@sv.cmu.edu>
2014-04-17 00:52:14 -0700

    Naive bayes fuzzy indicator function

--- ml/scratch.py
+++ ml/scratch.py
@@ -7,6 +7,7 @@ __version__='Sp2014'
 
 import config
 from db.scratch import *
+import trust.scratch as trust
 
 import math
 import pymongo
@@ -50,6 +51,51 @@ def get_coauthor_distance_before_year(col, primary_author, coauthor, year):
         if coauthor in doc2['author']:
           return 2
   return 999
+
+def write_line_for_authors(col, author1, author2, year):
+  """
+  Decides whether each feature falls into H, M, or L regions for given authors. Returns results separated by commas.
+  """
+  coauthor_distance = get_coauthor_distance_before_year(col, author1, author2, year)
+  trust_value_difference = trust.trust_value(col, author1, year) - trust.trust_value(col, author1, year)
+  jaccard_val = jaccard_similarity(col, author1, author2)
+  coauthorship_history1 = num_coauthors_in_range(col, author1, end=year)
+  coauthorship_history2 = num_coauthors_in_range(col, author2, end=year)
+  effective_coauthorship = min(coauthorship_history1, coauthorship_history2)
+  line = ""
+  if coauthor_distance == 1:
+    cd_line = "Y"
+  else:
+    cd_line = "N"
+  #Needs to be tuned  
+  if jaccard_val >= 0.5:
+    j_line = "H"
+  elif jaccard_val < 0.5 and jaccard_val >= 0.25:
+    j_line = "M"
+  else:
+    j_line = "L"
+
+  if coauthor_distance <= 1:
+    cd_line2 = "H"
+  elif coauthor_distance <=2 and coauthor_distance > 1:
+    cd_line2 = "M"
+  else:
+    cd_line2 = "L"
+
+  if trust_value_difference < 0:
+    tv_line = "H"
+  elif trust_value_difference >=0 and trust_value_difference <= 1:
+    tv_line = "M"
+  else:
+    tv_line = "L"
+
+  if effective_coauthorship > 5:
+    ec_line = "H"
+  elif effective_coauthorship <= 5 and effective_coauthorship > 1:
+    ec_line = "M"
+  else:
+    ec_line = "L"
+  return "{0},{1},{2},{3},{4}".format(cd_line, j_line, cd_line2, tv_line, ec_line)
     
 if __name__ == '__main__':
   db = pymongo.MongoClient()[config.DB_NAME]
@@ -67,6 +113,7 @@ if __name__ == '__main__':
 #  print get_coauthor_distance_before_year(col, 'Luis Ramos', 'David J. DeWitt', 2013)
 # Should return 999
 #  print get_coauthor_distance_before_year(col, 'Luis Ramos', 'qqqqqqqqqqq', 2013)
+  print write_line_for_authors(col, 'Luis Ramos', 'Shahram Ghandeharizadeh', 2000)
 
   
 
