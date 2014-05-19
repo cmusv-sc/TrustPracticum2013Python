4b3af58a3255482a6cced1eac2aebcda83918336
Chris Lee <Chris.Lee@sv.cmu.edu>
2014-05-19 00:52:35 -0700

    *** empty log message ***

--- ml/bayes_prep.py
+++ ml/bayes_prep.py
@@ -9,9 +9,13 @@ if __name__ == '__main__':
   db = pymongo.MongoClient()[config.DB_NAME]
   col = db[config.COLLECTION_NAME]
   lst = []
+  if len(sys.argv) == 2:
+    year = int(sys.argv[1])
+  else:
+    year = 2014
   for name in sys.stdin:
     lst.append(name)
   for n1 in lst:
     for n2 in lst:
       if n1 != n2:
-        print write_line_for_authors(col, n1, n2, 2014)
+        print write_line_for_authors(col, n1, n2, year)
